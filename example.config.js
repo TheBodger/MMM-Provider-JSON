37f1ea4f281e00cb953a2662cfa438d53c2fcbd7
TheBodger <41484656+TheBodger@users.noreply.github.com>
2020-07-08 19:43:24 +0100

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -4,7 +4,7 @@
         consumerids: ["arrivalsBOS",], //mandatory ID of the consumer receiving the data from the module
         id: 'FlightArrivalsBOS', //mandatory unique ID
         package: 'FlightArrivals', //name of the package that contains a standard set of config details
-        urlparams: { apikey: 'enter your api key here', airportcode: 'LHR' }, //parameters that are embedded into the baseURL, such as an api key and the airport of interest (Heathrow)
+        urlparams: { apikey: 'enter your api key here', airportcode: 'BOS' }, //parameters that are embedded into the baseURL, such as an api key and the airport of interest (Heathrow)
         filename: 'flightsarrivingLHR.json', //the name of an output file containing the details sent to the consumer for debug usage etc
 
 },
