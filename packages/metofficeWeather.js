e94ef7f1eb551c654c29889926a3e24e0caa633c
TheBodger <41484656+TheBodger@users.noreply.github.com>
2022-07-18 07:48:15 +0100

    Update metofficeWeather.js

--- packages/metofficeWeather.js
+++ packages/metofficeWeather.js
@@ -1,52 +1,4 @@
 
-
-
-//http://api.aviationstack.com/v1/flights?access_key={apikey}&arr_iata={airportcode}
-
-//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-//http://api.openweathermap.org/geo/1.0/direct?q=Ascot&limit=5&appid=3508e37c895681628e59eb77bc181b49
-
-//old call formats - still supported
-//https://api.openweathermap.org/data/2.5/weather?id=2656992&units=metric&lang=en&APPID=e7c8f92f8c281a8679abe84c37d4883c
-//https://api.openweathermap.org/data/2.5/forecast?id=2656992&units=metric&lang=en&APPID=3508e37c895681628e59eb77bc181b49
-
-//new call formats
-//https://api.openweathermap.org/data/2.5/weather?lat=51.4062&lon=-0.6756&appid=3508e37c895681628e59eb77bc181b49
-//http://api.openweathermap.org/data/2.5/forecast?lat=51.4101286&lon=-0.6680194&appid=3508e37c895681628e59eb77bc181b49
-
-
-//51.4062° N, 0.6756° W
-//"lat": 51.4101286,
-//"lon": -0.6680194,
-
-
-////config settings:
-//{
-//	module: "MMM-Provider-JSON",
-//		config: {
-//		consumerids: ["testConsumer",], //mandatory ID of the consumer receiving the data from the module
-//		id: 'openweatherAscot', //mandatory unique ID
-//		package: 'OpenWeather', //name of the package that contains a standard set of config details
-//		urlparams: { appID: '3508e37c895681628e59eb77bc181b49', locationID: '2656992' },
-//		filename: 'openweather.json', //the name of an output file containing the details sent to the consumer for debug usage etc
-//		}
-//},
-
-//{
-//	module: "MMM-Provider-JSON",
-//		config: {
-//		consumerids: ["testConsumer",], //mandatory ID of the consumer receiving the data from the module
-//		id: 'metofficeAscot', //mandatory unique ID
-//		package: 'metofficeWeather', //name of the package that contains a standard set of config details
-//		urlparams: { appID: '2acecf0e-fb12-4870-b1e0-975b728172f7', locationID: '350153' },
-//		filename: 'openweather.json', //the name of an output file containing the details sent to the consumer for debug usage etc
-//		}
-//},
-
-//		//		apiBase: "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/",
-		//		locationID: 350153, //Ascot http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=2acecf0e-fb12-4870-b1e0-975b728172f7
-		//		apiKey: "2acecf0e-fb12-4870-b1e0-975b728172f7",
-//http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/3840?res=3hourly&key=01234567-89ab-cdef-0123-456789abcdef
 var package =
 {
 	type: 'metOfficeWeather', //mandatory //no spaces
@@ -55,8 +7,7 @@ var package =
 	itemstype: 'array', //default is array
 	input:'URL',
 	fields: [//mandatory
-		//2022-07-15Z
-		{ name: { key: true, outputname: 'location', } }, //as this is the key it will be output as subject
+		{ name: { key: true, outputname: 'location', } }, //as this is the key it will be output as subject // this doesnt pick up location but forces no object in output
 		{ value: { outputname: 'date', inputtype: 't', timestampformat: 'YYY-MM-DD ', outputtype: 't' } }, //as this is the key it will be output as subject
 		{ $: { address: 'Rep[]', inputtype: 'n', outputname: 'minutesPastmidnight', } },// Rep is an array so we need to loop through the data generating records for each one 
 		{ G: { address: 'Rep[]', inputtype: 'n', outputname: 'windGust', } }, 
@@ -69,7 +20,7 @@ var package =
 
 }
 
-//where weathercode:
+//where weathercode returned is in:
 //NA Not available
 //0 Clear night
 //1 Sunny day
